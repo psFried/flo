8611df37efa8157c0d73c194a55a8ffa0025e708
pfried <philipsfried@gmail.com>
2016-05-03 22:42:27 -0400

    changed index into a ring buffer, get_next_entry tests still failing

--- src/event_store/event_index.rs
+++ src/event_store/event_index.rs
@@ -27,6 +27,7 @@ pub struct EventIndex {
     drop_to_index: usize,
     max_num_events: usize,
     head_index: usize,
+    head_event_id: EventId,
 }
 
 impl EventIndex {
@@ -41,6 +42,7 @@ impl EventIndex {
             drop_to_index: 0,
             max_num_events: max_size,
             head_index: 0,
+            head_event_id: 0,
         };
         index.fill_entries();
         index
@@ -52,13 +54,19 @@ impl EventIndex {
 
     pub fn add(&mut self, event_id: EventId, offset: usize) {
         self.num_entries += 1;
-        self.max_event = event_id;
-        let idx = self.ensure_capacity_for(event_id);
+        let idx = EventIndex::get_relative_index(self.head_index, self.head_event_id, event_id, self.max_num_events);
+        self.ensure_capacity(idx);
         self.entries[idx] = Some(Entry::new(event_id, offset));
+        self.set_new_head(event_id, idx);
     }
 
     pub fn get(&self, event_id: EventId) -> Option<Entry> {
-        None
+        let index = self.get_index(event_id);
+        if index < self.entries.len() {
+            self.entries[index]
+        } else {
+            None
+        }
     }
 
     pub fn get_next_entry(&mut self, event_id: EventId) -> Option<Entry> {
@@ -69,41 +77,11 @@ impl EventIndex {
         self.drop_to_event = min_event_id;
     }
 
-
-    fn can_grow(&self) -> bool {
-        self.entries.len() < self.max_num_events
-    }
-
-    fn grow_storage(&mut self, ensure_index: usize) {
-        let current_len = self.entries.len();
-
-    }
-
-    fn get_new_capacity(&self, ensure_index: usize) -> usize {
-        unimplemented!()
-    }
-
-    fn drop_entries(&mut self) {
-
-    }
-
-
-    fn ensure_capacity_for(&mut self, event_id: EventId) -> usize {
-        match self.entries[self.head_index] {
-            Some(entry) => {
-                let head_event_id = entry.event_id;
-                if head_event_id > event_id {
-                    0
-                } else {
-                    0
-                }
-            },
-            None => self.head_index
+    fn set_new_head(&mut self, event_id: EventId, index: usize) {
+        if event_id > self.head_event_id {
+            self.head_index = index;
+            self.head_event_id = event_id;
         }
-    }
-
-    fn should_drop(&self) -> bool {
-        self.drop_to_event > self.min_event
     }
 
     fn fill_entries(&mut self) {
@@ -113,8 +91,19 @@ impl EventIndex {
         }
     }
 
+    fn ensure_capacity(&mut self, index: usize) {
+        let current_len = self.entries.len();
+        if index >= current_len {
+            let new_capacity = index + (index as f64 * LOAD_FACTOR) as usize;
+            let max_additional = self.max_num_events - current_len;
+            let additional_capacity = ::std::cmp::min(new_capacity - current_len, max_additional);
+            self.entries.reserve_exact(additional_capacity);
+            self.fill_entries();
+        }
+    }
+
     fn get_index(&self, event_id: EventId) -> usize {
-        (event_id - self.min_event - 1) as usize
+        EventIndex::get_relative_index(self.head_index, self.head_event_id, event_id, self.max_num_events)
     }
 
     fn get_relative_index(head_index: usize,
@@ -122,9 +111,20 @@ impl EventIndex {
         new_event_id: EventId,
         max_exclusive: usize) -> usize {
 
-            0
-
+        if new_event_id > head_event_id {
+            let event_id_diff = (new_event_id - head_event_id) as usize;
+            let index = head_index + event_id_diff;
+            index % max_exclusive
+        } else {
+            let event_id_diff = (head_event_id - new_event_id) as usize;
+            if event_id_diff > head_index {
+                let wrapping_amount = event_id_diff - head_index;
+                max_exclusive - wrapping_amount
+            } else {
+                head_index - event_id_diff
+            }
         }
+    }
 }
 
 
@@ -138,7 +138,73 @@ mod test {
     }
 
     #[test]
-    fn get_relative_index_add_difference_between_head_and_new_event_ids_if_under_max() {
+    fn old_entries_are_overwritten_once_max_num_entries_is_reached() {
+        let max_capacity = 50usize;
+        let mut index = EventIndex::new(10, 50);
+
+        for i in 1..(max_capacity as u64 + 75) {
+            index.add(i, 30 * i as usize);
+        }
+        assert_eq!(max_capacity, index.entries.capacity());
+        assert_eq!(124, index.head_event_id);
+        assert_eq!(24, index.head_index);
+    }
+
+    #[test]
+    fn adding_an_entry_increases_the_capacity_of_the_storage_if_needed() {
+        let initial_capacity = 5;
+        let mut index = EventIndex::new(initial_capacity, 100);
+        assert_eq!(initial_capacity, index.entries.capacity());
+
+        index.add(10, 777);
+
+        assert!(index.entries.capacity() > 10);
+    }
+
+    #[test]
+    fn get_relative_index_wraps_around_0_when_new_event_id_is_less_than_head_id() {
+        let head_index = 1;
+        let head_event_id: EventId = 555;
+        let new_event_id: EventId = 550;
+        let max = 1000;
+
+        let result = EventIndex::get_relative_index(head_index,
+                head_event_id,
+                new_event_id,
+                max);
+        assert_eq!(996, result);
+    }
+
+    #[test]
+    fn get_relative_index_wraps_around_max_value_when_new_event_id_is_greater_than_head_id() {
+        let head_index = 8;
+        let head_event_id: EventId = 555;
+        let new_event_id: EventId = 557;
+        let max = 10;
+
+        let result = EventIndex::get_relative_index(head_index,
+                head_event_id,
+                new_event_id,
+                max);
+        assert_eq!(0, result);
+    }
+
+    #[test]
+    fn get_relative_index_subtracts_difference_when_new_event_id_is_less_than_head_event_id() {
+        let head_index = 10;
+        let head_event_id: EventId = 66;
+        let new_event_id: EventId = 60;
+        let max = 9999;
+
+        let result = EventIndex::get_relative_index(head_index,
+                head_event_id,
+                new_event_id,
+                max);
+        assert_eq!(4, result);
+    }
+
+    #[test]
+    fn get_relative_index_adds_difference_between_head_and_new_event_ids_if_under_max() {
         let head_index = 0;
         let head_event_id: EventId = 1;
         let new_event_id: EventId = 2;
@@ -149,16 +215,6 @@ mod test {
                 new_event_id,
                 max);
         assert_eq!(1, result);
-    }
-
-    #[test]
-    fn adding_an_entry_should_reallocate_if_new_capacity_is_needed() {
-        let initial_size = 5;
-        let mut index = EventIndex::new(initial_size, 100);
-
-        index.add(75, 8888);
-        assert_eq!(Some(Entry::new(75, 8888)), index.get(75));
-        assert!(index.entries.len() >= 75);
     }
 
     #[test]
@@ -177,14 +233,10 @@ mod test {
     }
 
     #[test]
-    fn the_capacity_and_length_of_the_internal_storage_vector_are_always_the_same() {
-        let mut index = new_index();
-        assert_eq!(index.entries.capacity(), index.entries.len());
-
-        for i in 1..70 {
-            index.add(i, 40 * i as usize);
-        }
-        assert_eq!(index.entries.capacity(), index.entries.len());
+    fn get_returns_none_when_event_id_is_out_of_bounds() {
+        let mut idx = new_index();
+        let result = idx.get(987654);
+        assert!(result.is_none());
     }
 
     #[test]
@@ -200,7 +252,7 @@ mod test {
     }
 
     #[test]
-    fn adding_an_entry_increases_length_by_one() {
+    fn adding_an_entry_increases_num_entries_by_one() {
         let mut idx = new_index();
         assert_eq!(0, idx.num_entries());
         idx.add(1, 886734);
