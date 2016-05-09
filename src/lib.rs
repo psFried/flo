b2a53fc68487da08520acee0502179637468a17b
pfried <philipsfried@gmail.com>
2016-05-08 22:38:19 -0400

    finished creating a place for client code to live

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,5 @@
-extern crate rotor;
-extern crate rotor_http;
-extern crate netbuf;
+extern crate hyper;
 extern crate serde_json;
-extern crate queryst;
-extern crate lru_time_cache;
 
 #[macro_use]
 extern crate log;
-
-#[cfg(test)]
-extern crate httparse;
-
-#[cfg(test)]
-extern crate tempdir;
-
-pub mod server;
-pub mod context;
-pub mod event_store;
-pub mod event;
-
-#[cfg(test)]
-mod test_utils;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 extern crate flo;
 extern crate log4rs;
+
+#[macro_use]
 extern crate log;
+extern crate rotor;
+extern crate rotor_http;
+extern crate netbuf;
+extern crate serde_json;
+extern crate queryst;
+extern crate lru_time_cache;
+
+#[cfg(test)]
+extern crate httparse;
+
+#[cfg(test)]
+extern crate tempdir;
+
+
+pub mod server;
+pub mod context;
+pub mod event_store;
+pub mod event;
 
 mod logging;
+
+#[cfg(test)]
+mod test_utils;
+
 
 use logging::init_logging;
 
 fn main() {
     init_logging();
-    flo::server::start_server();
+    server::start_server();
 }
