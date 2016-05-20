7d207ebb23e227fb92223f4dea6aa32c0fc74968
pfried <philipsfried@gmail.com>
2016-05-20 18:20:48 -0400

    start a new server instance for each integration test with its own port and data directory

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,7 @@
 extern crate flo;
 extern crate url;
 extern crate env_logger;
+extern crate tempdir;
 
 
 use url::Url;
@@ -10,7 +11,11 @@ use std::process::{Child, Command};
 use std::thread;
 use std::time::Duration;
 use std::sync::{Once, ONCE_INIT};
+use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+use std::borrow::Borrow;
+use std::path::Path;
 
+static mut PORT: AtomicUsize = ATOMIC_USIZE_INIT;
 
 static ON_START: Once = ONCE_INIT;
 
@@ -25,15 +30,23 @@ pub struct FloServerProcess {
 }
 
 impl FloServerProcess {
-    pub fn new() -> FloServerProcess {
+    pub fn new(port: u16, data_dir: &Path) -> FloServerProcess {
         use std::path::PathBuf;
         use std::env::current_dir;
 
         let mut flo_path = current_dir().unwrap();
         flo_path.push("target/debug/flo");
-        println!("Starting flo server");
-        let child = Command::new(flo_path).spawn().unwrap();
 
+        println!("Starting flo server");
+        let child = Command::new(flo_path)
+                .arg("--port")
+                .arg(format!("{}", port))
+                .arg("--data-dir")
+                .arg(data_dir)
+                .spawn().unwrap();
+
+        // hack to prevent test from starting until the server is actually started
+        // TODO: wait for log output that indicates server is ready
         thread::sleep(Duration::from_millis(500));
         FloServerProcess {
             child_proc: Some(child)
@@ -53,28 +66,31 @@ impl Drop for FloServerProcess {
 }
 
 macro_rules! integration_test {
-    ($d:ident, $t:block) => (
+    ($d:ident, $p:ident, $t:block) => (
         #[test]
         fn $d() {
             init_logger();
-            let mut flo_server_proc = FloServerProcess::new();
+            let port =  unsafe {
+                3000u16 + PORT.fetch_add(1, Ordering::Relaxed) as u16
+            };
+            let data_dir = tempdir::TempDir::new("flo-integration-test").unwrap();
+            let mut flo_server_proc = FloServerProcess::new(port, data_dir.path());
+            let $p: Url = Url::parse(&format!("http://localhost:{}", port)).unwrap();
             $t
         }
     )
 }
 
 
-integration_test!(producer_produces_event_and_gets_event_id_in_response, {
-    let url = Url::parse("http://localhost:3000").unwrap();
-    let mut producer = FloProducer::new(url);
+integration_test!(producer_produces_event_and_gets_event_id_in_response, server_url, {
+    let mut producer = FloProducer::new(server_url);
     let result = producer.emit_raw(r#"{"myKey": "what a great value!"}"#);
     assert!(result.is_ok());
     assert_eq!(1, result.unwrap());
 });
 
-integration_test!(producer_emits_multiple_events_and_returns_iterator_of_results, {
-    let url = Url::parse("http://localhost:3000").unwrap();
-    let mut producer = FloProducer::new(url);
+integration_test!(producer_emits_multiple_events_and_returns_iterator_of_results, server_url, {
+    let mut producer = FloProducer::new(server_url);
 
     let events = vec![
         ObjectBuilder::new().insert("keyA", "valueA").unwrap(),
