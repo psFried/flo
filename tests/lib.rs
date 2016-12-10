3be520f57d9bb1c146a969f0a3e3a17f4e7621ff
pfried <philipsfried@gmail.com>
2016-12-10 18:32:54 -0500

    add failing integration test for consuming persisted events

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,11 +1,16 @@
 extern crate flo;
+extern crate flo_event;
 extern crate url;
 extern crate env_logger;
 extern crate tempdir;
+extern crate byteorder;
 
+#[macro_use]
+extern crate nom;
 
+use nom::{be_u64, be_u32, be_u16};
 use url::Url;
-use flo::event::{EventId, Event};
+use flo_event::{FloEventId, OwnedFloEvent, FloEvent};
 use std::process::{Child, Command};
 use std::thread;
 use std::time::Duration;
@@ -14,53 +19,72 @@ use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
 use std::path::Path;
 use std::net::{TcpStream, SocketAddr, SocketAddrV4, IpAddr, Ipv4Addr};
 use std::io::{Write, Read};
+use byteorder::{ByteOrder, BigEndian};
 
 //TODO: get integration tests building again
 
 macro_rules! integration_test {
-    ($d:ident, $p:ident, $t:block) => (
+    ($d:ident, $p:ident, $s:ident, $t:block) => (
         #[test]
         #[allow(unused_variables)]
         fn $d() {
             init_logger();
-            let port_var = ::std::env::var("FLO_TEST_PORT").map(|value| {
+            let port_var = ::std::env::var("FLO_TEST_PORT").ok().map(|value| {
                 value.parse::<u16>().unwrap()
             });
 
-            if let Ok(port) = port_var {
-                let $p = port;
-                $t
-            } else {
-                let $p = unsafe {
-                    3000u16 + PORT.fetch_add(1, Ordering::Relaxed) as u16
-                };
+            let port = port_var.unwrap_or_else(|| {
+                unsafe {
+                    3001u16 + PORT.fetch_add(1, Ordering::Relaxed) as u16
+                }
+            });
+
+            let mut flo_proc: Option<FloServerProcess> = None;
+            if port_var.is_none() {
+                // if env variable is not defined, then we need to start the server process ourselves
                 let data_dir = tempdir::TempDir::new("flo-integration-test").unwrap();
-                let flo_server_proc = FloServerProcess::new($p, data_dir.path());
-                $t
+                flo_proc = Some(FloServerProcess::new(port, data_dir.path()));
+                // To give the external process time to start
+                thread::sleep(Duration::from_millis(250));
             }
+
+
+            let address: SocketAddr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port));
+            let mut $s = TcpStream::connect(address).unwrap();
+
+            let $p = port;
+
+            $t
         }
     )
 }
 
-integration_test!{event_is_written_and_ackgnowledged, server_port, {
-    let address: SocketAddr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), server_port));
-    let mut tcp_stream = TcpStream::connect(address).unwrap();
-
-    tcp_stream.write_all(b"FLO_PRO\n").unwrap();
-    tcp_stream.write_all(&[0, 1, 2, 3]).unwrap();
-    tcp_stream.write_all(&[0, 0, 0, 9]).unwrap();
-    tcp_stream.write_all(b"ninechars").unwrap();
+integration_test!{event_is_written_and_ackgnowledged, _port, tcp_stream, {
+    let event_data = b"ninechars";
+    let op_id = produce_event(&mut tcp_stream, &event_data[..]);
 
     let mut buff = [0; 1024];
     let nread = tcp_stream.read(&mut buff).unwrap();
 
-    println!("read {} bytes: {:?}", nread, &buff[..nread]);
-
     let expected = b"FLO_ACK\n";
     assert_eq!(&expected[..], &buff[..8]);      //header
-    assert_eq!(&[0, 1, 2, 3], &buff[8..12]);    //op_id
+    let result_op_id = BigEndian::read_u32(&buff[8..12]);
+    assert_eq!(op_id, result_op_id);
     assert_eq!(&[0, 1, 0, 0, 0, 0, 0, 0, 0, 1], &buff[12..22]);//event id (actor: u16, event_counter: u64)
+}}
 
+integration_test!{persisted_event_are_consumed_after_they_are_written, server_port, tcp_stream, {
+    let event1_data = b"first event data";
+    produce_event(&mut tcp_stream, &event1_data[..]);
+
+    let event2_data = b"second event data";
+    produce_event(&mut tcp_stream, &event2_data[..]);
+
+    let mut consumer = connect(server_port);
+    consumer.write_all(b"FLO_CNS\n").unwrap();
+    consumer.write_all(&[0, 0, 0, 2]).unwrap();
+
+    let results = read_events(&mut consumer, 2);
 }}
 
 
@@ -68,6 +92,79 @@ integration_test!{event_is_written_and_ackgnowledged, server_port, {
 ///////  Test Utils            ////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
+
+
+named!{parse_event<OwnedFloEvent>,
+    chain!(
+        _tag: tag!("FLO_EVT\n") ~
+        counter: be_u64 ~
+        actor: be_u16 ~
+        data: length_bytes!(be_u32),
+        || {
+            OwnedFloEvent {
+                id: FloEventId::new(actor, counter),
+                namespace: String::new(),
+                data: data.to_owned()
+            }
+        }
+    )
+}
+
+fn read_events(tcp_stream: &mut TcpStream, mut nevents: usize) -> Vec<OwnedFloEvent> {
+    use nom::IResult;
+
+    let mut events = Vec::new();
+    let mut buffer = [0; 8 * 1024];
+    let mut buffer_start = 0;
+    let mut nread = tcp_stream.read(&mut buffer[buffer_start..]).unwrap();
+    while nevents > 0 {
+        let shift_amt = match parse_event(&buffer[buffer_start..nread]) {
+            IResult::Done(remaining, event) => {
+                events.push(event);
+                remaining.len()
+            }
+            IResult::Error(err) => panic!("Error deserializing event: {:?}", err),
+            IResult::Incomplete(need) => {
+                panic!("Incomplete data to read events: {:?}", need)
+            }
+        };
+
+        if shift_amt > 0 {
+            for i in 0..shift_amt {
+
+                buffer.swap(i, i + (nread - shift_amt));
+            }
+        }
+        buffer_start = nread - shift_amt;
+    }
+    events
+}
+
+fn connect(port: u16) -> TcpStream {
+    let address: SocketAddr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port));
+    let mut stream = TcpStream::connect(address).unwrap();
+    stream.set_read_timeout(Some(Duration::from_millis(500)));
+    stream
+}
+
+static mut OP_ID: AtomicUsize = ATOMIC_USIZE_INIT;
+
+fn produce_event(tcp_stream: &mut TcpStream, bytes: &[u8]) -> u32 {
+
+    let op_id = unsafe {
+        OP_ID.fetch_add(1, Ordering::SeqCst) as u32
+    };
+    let mut buffer = [0; 4];
+    BigEndian::write_u32(&mut buffer[..], op_id);
+    tcp_stream.write_all(b"FLO_PRO\n").unwrap();
+    tcp_stream.write_all(&buffer).unwrap();
+
+    BigEndian::write_u32(&mut buffer[..], bytes.len() as u32);
+    tcp_stream.write_all(&buffer).unwrap();
+    tcp_stream.write_all(bytes).unwrap();
+
+    op_id
+}
 
 static mut PORT: AtomicUsize = ATOMIC_USIZE_INIT;
 
