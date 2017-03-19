4613db67f0800d650e021dba047ae88e9ece144c
pfried <philipsfried@gmail.com>
2017-03-19 12:29:06 -0400

    change EventCodec type to use a generic parameter for the body type instead of an associated type to allow for blanket impls

--- flo-client-lib/src/sync/basic.rs
+++ flo-client-lib/src/sync/basic.rs
@@ -9,12 +9,12 @@ pub use event::{FloEventId, Timestamp};
 pub use sync::connection::{Connection};
 
 /// Establishes a new basic connection using a binary (no-op) codec to the given address
-pub fn connect_with_binary_codec<T: ToSocketAddrs>(address: T) -> io::Result<Connection<RawCodec>> {
+pub fn connect_with_binary_codec<T: ToSocketAddrs>(address: T) -> io::Result<Connection<Vec<u8>, RawCodec>> {
     SyncConnection::connect(address, RawCodec)
 }
 
 /// Establishes a new connection using a String codec allowing events to be consumed and produced using strings as the body
-pub fn connect_with_string_codec<T: ToSocketAddrs>(address: T) -> io::Result<Connection<StringCodec>> {
+pub fn connect_with_string_codec<T: ToSocketAddrs>(address: T) -> io::Result<Connection<String, StringCodec>> {
     SyncConnection::connect(address, StringCodec)
 }
 
