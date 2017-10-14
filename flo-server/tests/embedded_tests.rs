194cf9a6165b5ab3bd43189603520d7374c2e410
pfried <philipsfried@gmail.com>
2017-10-13 22:33:13 -0400

    update dependencies, add failing test for consuming in new engine

--- flo-server/tests/embedded_tests.rs
+++ flo-server/tests/embedded_tests.rs
@@ -10,6 +10,8 @@ extern crate chrono;
 #[macro_use]
 extern crate log;
 
+use std::fmt::Debug;
+
 use tokio_core::reactor::Core;
 use futures::{Stream, Future};
 use chrono::Duration;
@@ -47,10 +49,25 @@ fn integration_test<F>(test_name: &'static str, stream_opts: EventStreamOptions,
     fun(embedded_server, reactor);
 }
 
+fn run_future<T: Debug, E: Debug, F: Future<Item=T, Error=E> + Debug>(reactor: &mut Core, future: F) -> T {
+    use tokio_core::reactor::Timeout;
+    use futures::future::Either;
 
+    let timeout_millis = 100;
+
+    let timeout = Timeout::new(::std::time::Duration::from_millis(timeout_millis), &reactor.handle()).unwrap();
+    let either_future = timeout.select2(future);
+    let either = reactor.run(either_future);
+    match either {
+        Ok(Either::B((value, _))) => value,
+        Ok(Either::A(((), _))) => panic!("future timed out after {} millis", timeout_millis),
+        Err(Either::A(_)) => panic!("Error in timeout"),
+        Err(Either::B((err, _))) => panic!("Error executing future: {:?}", err),
+    }
+}
 
 #[test]
-fn produce_one_event() {
+fn produce_one_event_then_consume_it() {
     integration_test("produce one event", default_test_options(), |server, mut reactor| {
         let client = server.connect_client::<String>("testy mctesterson".to_owned(), codec());
 
@@ -59,6 +76,14 @@ fn produce_one_event() {
 
         let (id, client) = reactor.run(future).expect("failed to run produce future");
         println!("produced event: {}", id);
+
+        let mut version_vec = VersionVector::new();
+        version_vec.set(FloEventId::new(1, 0));
+
+        let consume_future = client.consume("/foo/*", &version_vec, Some(1));
+        let mut event_result = run_future(&mut reactor, consume_future.collect());
+        assert_eq!(1, event_result.len());
+        assert_eq!(&event_result.pop().unwrap().data, "my data");
     });
 }
 
