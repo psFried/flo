51b3c58078e5ef90aa296f0d20561e958231ce1a
pfried <philipsfried@gmail.com>
2018-06-10 10:51:21 -0400

    minor improvements to dev-start-cluster.sh

--- dev-start-cluster.sh
+++ dev-start-cluster.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 USAGE=$(cat <<EOF
-Starts a cluster locally for dev purposes. Will first build all targets in debug mode.
+Starts a cluster locally for dev purposes
+
 Options: 
+-h Print this message and exit
 -d <dir> Sets the root directory, under which all data directories will be created
 -p <port> Starting number of a port range that instances will listen on. Will use ports sequentially from there.
 -a <arg> Adds an additional argument to all flo instances started
+-r Remove existing data directories and log files before starting the cluster
 -x Do not start any servers (will only print the flo commands that would have been run). Can be used to delete data and logs without starting servers
+-b Build all targets before starting cluster
 EOF
 )
 NUM_INSTANCES=3
@@ -15,6 +19,7 @@ DATA_ROOT="${TMPDIR}"
 DELETE_DATA_DIRS="no"
 ADDITIONAL_ARGS=()
 DRY_RUN="no"
+BUILD="no"
 
 PIDS=()
 
@@ -31,7 +36,6 @@ function start_instance() {
     shift
     local peers="$@"
     peers=($(echo "${peers[@]//$instance_num}"))
-    stderr "instance_num: ${instance_num} filtered peers: ${peers[@]}"
 
     local peer_ports=()
     for peer in "${peers[@]}"; do
@@ -42,16 +46,13 @@ function start_instance() {
     local this_addr="${IP_ADDR}:${this_port}"
     local peer_addr_args="${peer_ports[@]/#/-P $IP_ADDR:}"
 
-    stderr "this_port='${this_port}', this_addr='${this_addr}', peer_args='${peer_addr_args}'"
     local command="${FLO_EXE_PATH} -d ${DATA_ROOT}/flo${instance_num} -A ${this_addr} -p ${this_port} ${peer_addr_args} ${ADDITIONAL_ARGS[@]} -o ${DATA_ROOT}/flo${instance_num}.log"
     stderr "command: $command"
 
-    if [[ "${DRY_RUN}" == "no" ]]; then
-        $command &
-        local pid="$!"
-        PIDS+=("${pid}")
-        stderr "Started flo process with pid: ${pid}"
-    fi
+    $command &
+    local pid="$!"
+    PIDS+=("${pid}")
+    stderr "Started flo process with pid: ${pid}"
 }
 
 function delete_data_dirs() {
@@ -81,6 +82,7 @@ function tail_logs() {
 }
 
 function stop_all() {
+    echo ""
     for pid in ${PIDS[@]}; do
         stderr "Stopping flo pid: ${pid}"
         kill "$pid" || true
@@ -98,19 +100,24 @@ function start_all() {
         delete_data_dirs "${all_peers[@]}"
     fi
 
-    for instance_num in "${all_peers[@]}"; do
-        stderr "loop instance num: ${instance_num}"
-        start_instance "$instance_num" "${all_peers[@]}"
-    done
-
     if [[ "${DRY_RUN}" == "no" ]]; then
+        for instance_num in "${all_peers[@]}"; do
+            start_instance "$instance_num" "${all_peers[@]}"
+        done
+
+        # Sleep for a short bit to make sure that the log files actually exist before we start tailing them
+        sleep 1
         tail_logs "${all_peers[@]}"
     fi
 }
 
 
-while getopts "n:p:d:a:rx" opt; do
+while getopts "n:p:d:a:rxbh" opt; do
     case $opt in
+        b)
+            BUILD="yes"
+            stderr "Will build before running"
+            ;;
         n)
             NUM_INSTANCES="$OPTARG"
             stderr "Starting ${num_instances}" >&2
@@ -135,11 +142,28 @@ while getopts "n:p:d:a:rx" opt; do
             DRY_RUN="yes"
             stderr "Will be a dry run"
             ;;
+        h)
+            echo "${USAGE}"
+            exit 0
+            ;;
         \?)
-        stderr "Invalid option: -$OPTARG" >&2
-        ;;
+            stderr "Invalid option: -$OPTARG" >&2
+            stderr "${USAGE}"
+            exit 1
+            ;;
     esac
 done
+
+function do_build() {
+    local start_dir="$(pwd)"
+    cd "${SCRIPT_DIR}"
+    cargo build --all
+    cd "${start_dir}"
+}
+
+if [[ "${BUILD}" == "yes" ]]; then
+    do_build
+fi
 
 trap stop_all INT
 start_all
